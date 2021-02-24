5c7b02e16f3a380b2f27cbeae25f05172a37c0fb
alvinrapada <alvin@pragtechnologies.com>
2021-02-24 11:19:00 +0800

    Adds Documentation for Conversation APIs

--- lib/twilixir.ex
+++ lib/twilixir.ex
@@ -6,27 +6,33 @@ defmodule Twilixir do
   @base_twilio_url "https://conversations.twilio.com/v1/Conversations/"
   @doc """
   Creates a new conversation
+
+  Parameters
+  - friendly_name: string
+  - attrs: map (you can put any field you want for you own needs)
+
   ## Examples
-  iex> Twilixir.create_conversation("Alvin - Sample", %{status: "active"})
-  %{
-    "account_sid" => "AXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    "attributes" => "{\"status\":\"active\"}",
-    "chat_service_sid" => "CXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    "date_created" => "2021-02-24T02:04:36Z",
-    "date_updated" => "2021-02-24T02:04:36Z",
-    "friendly_name" => "Alvin - Sample",
-    "links" => %{
-      "messages" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Messages",
-      "participants" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Participants",
-      "webhooks" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Webhooks"
-    },
-    "messaging_service_sid" => "MXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    "sid" => "CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    "state" => "active",
-    "timers" => %{},
-    "unique_name" => nil,
-    "url" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-  }
+
+      iex> Twilixir.create_conversation("Alvin - Sample", %{status: "active"})
+      %{
+        "account_sid" => "AXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "attributes" => "{\"status\":\"active\"}",
+        "chat_service_sid" => "CXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "date_created" => "2021-02-24T02:04:36Z",
+        "date_updated" => "2021-02-24T02:04:36Z",
+        "friendly_name" => "Alvin - Sample",
+        "links" => %{
+          "messages" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Messages",
+          "participants" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Participants",
+          "webhooks" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Webhooks"
+        },
+        "messaging_service_sid" => "MXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "sid" => "CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "state" => "active",
+        "timers" => %{},
+        "unique_name" => nil,
+        "url" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+      }
 
   """
   def create_conversation(friendly_name, attrs) do
@@ -41,37 +47,44 @@ defmodule Twilixir do
   end
 
   @doc """
-  Get a conversation
+  Gets a conversation
+
+  Parameters
+  - conversation_sid: coversation's sid
+
   ## Valid Param
-  iex> Twilixir.get_conversation("SIDXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-    %{
-      "account_sid" => "AXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-      "attributes" => "{\"status\":\"active\"}",
-      "chat_service_sid" => "ISXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-      "date_created" => "2021-02-24T02:04:36Z",
-      "date_updated" => "2021-02-24T02:04:36Z",
-      "friendly_name" => "Alvin - Sample",
-      "links" => %{
-        "messages" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Messages",
-        "participants" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Participants",
-        "webhooks" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Webhooks"
-      },
-      "messaging_service_sid" => "MXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-      "sid" => "SIDXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-      "state" => "active",
-      "timers" => %{},
-      "unique_name" => nil,
-      "url" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-    }
+
+      iex> Twilixir.get_conversation("SIDXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+      %{
+        "account_sid" => "AXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "attributes" => "{\"status\":\"active\"}",
+        "chat_service_sid" => "ISXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "date_created" => "2021-02-24T02:04:36Z",
+        "date_updated" => "2021-02-24T02:04:36Z",
+        "friendly_name" => "Alvin - Sample",
+        "links" => %{
+          "messages" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Messages",
+          "participants" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Participants",
+          "webhooks" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Webhooks"
+        },
+        "messaging_service_sid" => "MXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "sid" => "SIDXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "state" => "active",
+        "timers" => %{},
+        "unique_name" => nil,
+        "url" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+      }
 
   ## Invalid Param
-  iex> Twilixir.get_conversation("INVALID_XXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-    %{
-      "code" => 20404,
-      "message" => "The requested resource /Conversations/INVALID_XXXXXXXXXXXXXXXXXXXXXXXXXXXXX was not found",
-      "more_info" => "https://www.twilio.com/docs/errors/20404",
-      "status" => 404
-    }
+
+      iex> Twilixir.get_conversation("INVALID_XXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+      %{
+        "code" => 20404,
+        "message" => "The requested resource /Conversations/INVALID_XXXXXXXXXXXXXXXXXXXXXXXXXXXXX was not found",
+        "more_info" => "https://www.twilio.com/docs/errors/20404",
+        "status" => 404
+      }
+
   """
   def get_conversation(conversation_sid) do
     api_url = "#{@base_twilio_url}#{conversation_sid}"
@@ -79,23 +92,160 @@ defmodule Twilixir do
     api_get_request(api_url)
   end
 
-  def get_multiple_conversations(page_size \\ 50) do
-    api_url = "#{@base_twilio_url}?PageSize=#{page_size}"
+  @doc """
+  Gets multiple conversations (with pagination)
+
+  Parameters
+  - page: current page
+  - page_size: Conversation Size Per Page
+
+  ## Examples
+    
+      iex> Twilixir.get_multiple_conversations(1, 2)
+      %{
+        "conversations" => [
+          %{
+            "account_sid" => "AXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            "attributes" => "{\"status\":\"active\"}",
+            "chat_service_sid" => "ISXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            "date_created" => "2021-02-24T02:04:36Z",
+            "date_updated" => "2021-02-24T02:04:36Z",
+            "friendly_name" => "Alvin - Sample",
+            "links" => %{
+              "messages" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Messages",
+              "participants" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Participants",
+              "webhooks" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Webhooks"
+            },
+            "messaging_service_sid" => "MXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            "sid" => "CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            "state" => "active",
+            "timers" => %{},
+            "unique_name" => nil,
+            "url" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+          },
+          ...
+        ],
+        "meta" => %{
+          "first_page_url" => "https://conversations.twilio.com/v1/Conversations?PageSize=2&Page=0",
+          "key" => "conversations",
+          "next_page_url" => "https://conversations.twilio.com/v1/Conversations?PageSize=2&Page=2&PageToken=PXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+          "page" => 1,
+          "page_size" => 2,
+          "previous_page_url" => nil,
+          "url" => "https://conversations.twilio.com/v1/Conversations?PageSize=2&Page=1"
+        }
+      }
+
+  """
+  def get_multiple_conversations(page \\ 1, page_size \\ 50) do
+    api_url = "#{@base_twilio_url}?Page=#{page}&PageSize=#{page_size}"
 
     api_get_request(api_url)
   end
 
-  def update_conversation(conversation_sid, new_attrs) do
+  @doc """
+  Gets all conversations
+
+  ## Examples
+
+      iex> Twilixir.get_all_conversations()
+      %{
+        "conversations" => [
+          %{
+            "account_sid" => "AXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            "attributes" => "{\"status\":\"active\"}",
+            "chat_service_sid" => "ISXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            "date_created" => "2021-02-24T02:04:36Z",
+            "date_updated" => "2021-02-24T02:04:36Z",
+            "friendly_name" => "Alvin - Sample",
+            "links" => %{
+              "messages" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Messages",
+              "participants" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Participants",
+              "webhooks" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Webhooks"
+            },
+            "messaging_service_sid" => "MXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            "sid" => "CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            "state" => "active",
+            "timers" => %{},
+            "unique_name" => nil,
+            "url" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+          },
+          ...
+        ],
+        "meta" => %{
+          "first_page_url" => "https://conversations.twilio.com/v1/Conversations?PageSize=PageSize&Page=0",
+          "key" => "conversations",
+          "next_page_url" => "https://conversations.twilio.com/v1/Conversations?PageSize=PageSize&Page=1&PageToken=PXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+          "page" => 0,
+          "page_size" => page_size,
+          "previous_page_url" => nil,
+          "url" => "https://conversations.twilio.com/v1/Conversations?PageSize=PageSize&Page=0"
+        }
+      }
+
+  """
+  def get_all_conversations do
+    api_url = "#{@base_twilio_url}"
+
+    api_get_request(api_url)
+  end
+
+  @doc """
+  Updates a conversation
+
+  Parameters
+  - conversation_sid: coversation's sid
+  - new attrs: map (you can put any field you want for you own needs)
+  - state: conversation state (active inactive close), note that once you close a conversation you can't re-activate it.
+
+  ## Examples
+
+      iex> Twilixir.update_conversation("CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", %{status: "inactive"}, "inactive")
+      %{
+        "account_sid" => "AXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "attributes" => "{\"status\":\"inactive\"}",
+        "chat_service_sid" => "ISXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "date_created" => "2021-02-24T02:04:36Z",
+        "date_updated" => "2021-02-24T02:36:34Z",
+        "friendly_name" => "Alvin - Sample",
+        "links" => %{
+          "messages" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Messages",
+          "participants" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Participants",
+          "webhooks" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Webhooks"
+        },
+        "messaging_service_sid" => "MXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "sid" => "CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "state" => "inactive",
+        "timers" => %{},
+        "unique_name" => nil,
+        "url" => "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+      }
+
+  """
+  def update_conversation(conversation_sid, new_attrs, state) do
     api_url = "#{@base_twilio_url}#{conversation_sid}"
 
     body = %{
-      State: "active",
+      State: state,
       Attributes: new_attrs |> Jason.encode!()
     }
 
     api_post_request(api_url, body)
   end
 
+  @doc """
+  Deletes a conversation
+
+  Parameters
+  - conversation_sid: coversation's sid
+
+  ## Examples
+
+      iex> Twilixir.delete_conversation("CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+      Success - Deleted
+      :ok
+
+  """
   def delete_conversation(conversation_sid) do
     api_url = "#{@base_twilio_url}#{conversation_sid}"
     api_delete_request(api_url)
@@ -253,11 +403,13 @@ defmodule Twilixir do
   end
 
   @doc """
-    # NOTES:
-     - Assumes the phone number is a US number unless told otherwise
-     - Use two letter country codes - https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements
-     - You can look up the phone carrier info with '?Type=carrier'; BUT IT COSTS EXTRA
-     - You can attempt to lookup up the persons information with '?Type=caller-name'; BUT IT COSTS EXTRA
+  Validates a phone number
+
+  ## NOTES:
+   - Assumes the phone number is a US number unless told otherwise
+   - Use two letter country codes - https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements
+   - You can look up the phone carrier info with '?Type=carrier'; BUT IT COSTS EXTRA
+   - You can attempt to lookup up the persons information with '?Type=caller-name'; BUT IT COSTS EXTRA
   """
   def validate_mobile(phone_number, country_code) do
     api_url =
